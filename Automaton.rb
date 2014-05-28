eaaa556193c04d56f8c4005e3fe33a74299c3002
guatajuk <sp0t.col@gmail.com>
2014-05-27 23:35:12 -0500

    'Automaton.rb added, this automaton only has 1 state... not for long

---
+++ Automaton.rb
@@ -0,0 +1,27 @@
+class Automaton
+
+	attr_accessor :initial, :final, :stateList, :transitionMatrix, :size
+
+	def initialize
+		@size = 0
+		@initial = ""
+		@final = ""
+		@stateList = []
+		@transitionMatrix = []
+	end
+
+	def buildAutomaton
+		if @stateList.empty?
+			@initial = State.new
+			nameState(@initial)
+			@final = @initial
+			@stateList.push(@initial)
+		end
+	end
+
+	def nameState (state)
+		state.name = @size.to_s
+		@size++
+	end
+
+end
