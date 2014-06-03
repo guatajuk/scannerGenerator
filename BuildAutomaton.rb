49d8107bfd5fe946c3f1b80ed96efa70bdde69ef
Juan Camilo Noreña <jcnb81@gmail.com>
2014-06-03 14:28:13 -0500

    fixed == to = in buildautomaton class

--- BuildAutomaton.rb
+++ BuildAutomaton.rb
@@ -10,16 +10,20 @@ class BuildAutomaton
 	end
 
 	def build(regularExp)
-		terminal = regularExp.shift
-		case terminal
-		when '*'
-			kleene(terminal)
-		when '|'
-			union(terminal)
-		when '.'
-			concatenation(terminal)
-		else
-			@symbolStack.push(terminal)
+		until regularExp.size == 0
+			terminal = regularExp.shift
+		
+			case terminal
+			when '*'
+				kleene(terminal)
+			when '|'
+				union(terminal)
+			when '.'
+				concatenation(terminal)
+			else
+				@symbolStack.push(terminal)
+			end
+		end
 	end
 
 	def kleene(terminal)
@@ -30,13 +34,13 @@ class BuildAutomaton
 		automaton = Automaton.new(6)
 		unless @symbolStack.empty?
 			6.times { automaton.addState }
-			automaton.stateList.last.final == true
-			automaton.transitionMatrix[0][1] == "E"
-			automaton.transitionMatrix[0][3] == "E"
-			automaton.transitionMatrix[1][2] == @symbolStack[0]
-			automaton.transitionMatrix[2][5] == "E"
-			automaton.transitionMatrix[3][4] == @symbolStack[1]
-			automaton.transitionMatrix[4][5] == "E"
+			automaton.stateList.last.final = true
+			automaton.transitionMatrix[0][1] = "E"
+			automaton.transitionMatrix[0][3] = "E"
+			automaton.transitionMatrix[1][2] = @symbolStack[0]
+			automaton.transitionMatrix[2][5] = "E"
+			automaton.transitionMatrix[3][4] = @symbolStack[1]
+			automaton.transitionMatrix[4][5] = "E"
 		else
 			#implementar para hacer union entre dos automatas
 			#implementar para hacer union entre un automata y un simbolo del alfabeto
@@ -49,9 +53,9 @@ class BuildAutomaton
 		automaton = Automaton.new(3)
 		unless @symbolStack.empty?  
 			3.times { automaton.addState }
-			automaton.stateList.last.final == true
-			automaton.transitionMatrix[0][1] == @symbolStack[0]
-			automaton.transitionMatrix[1][2] == @symbolStack[1]
+			automaton.stateList.last.final = true
+			automaton.transitionMatrix[0][1] = @symbolStack[0]
+			automaton.transitionMatrix[1][2] = @symbolStack[1]
 		else
 			#implementar para hacer concatenación entre dos automatas
 			#implementar para hacer concatenación entre un automata y un simbolo del alfabeto
@@ -59,4 +63,5 @@ class BuildAutomaton
 		@automatonStack.push(automatonStack)
 		@symbolStack = []
 	end
+
 end
